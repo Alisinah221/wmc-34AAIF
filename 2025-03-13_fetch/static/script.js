e0522d513c8db1930d5d5da5fe2d331ce9413723
Alisinah221 <halisina74@gmail.com>
2025-03-16 19:13:36 +0100

    Homework

--- 2025-03-13_fetch/static/script.js
+++ 2025-03-13_fetch/static/script.js
@@ -1,37 +1,5 @@
-function isPrime(num) {
-    if (num <= 1) {
-            return false;
-        }
-    if (num <= 3) {
-        return true;
-    }
-
-    if (num % 2 === 0 || num % 3 === 0) {
-        return false;
-    }
-    for (let i = 5; i * i <= num; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function checkPrime() {
-    const num = document.getElementById("numberInput").value;
-    const resultElement = document.getElementById("result");
-    var regex= /^[0-9]+$/;
-    if(num.match(regex)){
-    if (isPrime(num)) {
-        resultElement.textContent = "Ja Es ist Primzahl!";
-        resultElement.style.color = "green";
-    } else {
-        resultElement.textContent = "Nein Keine Primzahl!";
-        resultElement.style.color = "red";
-    }
-    } else{
-        resultElement.textContent = "Üngultige Eingabe!";
-        resultElement.style.color = "red";
-    }
-}
+document.getElementById("fetchButton").addEventListener("click", async () => {
+    const response = await fetch("/api/data");
+    const data = await response.json();
+    document.getElementById("jsonOutput").value = JSON.stringify(data, null, 2);
+});
