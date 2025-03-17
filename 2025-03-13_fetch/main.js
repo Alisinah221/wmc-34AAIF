2b47803fbde42387035b21a1b84d169bcff317df
Alisinah221 <halisina74@gmail.com>
2025-03-17 19:32:42 +0100

    fetch

--- 2025-03-13_fetch/main.js
+++ 2025-03-13_fetch/main.js
@@ -1,15 +1,7 @@
-import { Hono } from "hono";
-import { serveStatic } from "hono/deno";
-
-const app = new Hono();
-const Animal = {Name:"NANA",
-                Type:"Mammal",
-                Color:"Black",
-                Age:3};
-// serve index.html
-app.get("/", serveStatic({ path: "./static/index.html" }));
-app.get("*", serveStatic({ root: "./static" }));
-app.get("/api/data", (c) => {
-    return c.json(Animal);
-  });
-Deno.serve(app.fetch);
+const Animal = {
+    name: "Leo",
+    age: 3,
+    hasTail: true,
+    color: "black",
+    breed: "Maine"
+};
--- 2025-03-13_fetch/static/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("fetchButton").addEventListener("click", async () => {
-    const response = await fetch("/api/data");
-    const data = await response.json();
-    document.getElementById("jsonOutput").value = JSON.stringify(data, null, 2);
-});
